8d52414e1d3651deb82267edb500f48e3aced75b
gopichand rudroju <rudrojugopichand@gmail.com>
2026-01-20 18:30:59 +0530

    defining jpa repositories

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -2,13 +2,19 @@ package com.ecommerce.project.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String categoryName;
+
+    public Category() {
+    }
 
     public Long getId() {
         return id;
