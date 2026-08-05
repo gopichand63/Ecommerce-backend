e18f6d7fe07164d5d8c36618e5dff9760f69f0e4
gopichand rudroju <rudrojugopichand@gmail.com>
2026-08-05 11:48:58 +0530

    Implemented websecurity configuration and userdetailsImpl services

--- src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -2,13 +2,14 @@ package com.ecommerce.project.security.services;
 
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.repositories.UserRepository;
-import com.ecommerce.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
